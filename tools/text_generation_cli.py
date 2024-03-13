40e427951b4cabb04cb343125081b6799bccc4c4
Risto Luukkonen <risto.m.luukkonen@gmail.com>
2024-03-13 15:05:45 +0200

    edit text cli

--- tools/text_generation_cli.py
+++ tools/text_generation_cli.py
@@ -8,20 +8,25 @@ if __name__ == "__main__":
     url = sys.argv[1]
     url = 'http://' + url + '/api'
     headers = {'Content-Type': 'application/json'}
-    sentence=""""\"\"\"\nWrite a function to convert the given binary tuple to integer.\nassert binary_to_integer((1, 1, 0, 1, 0, 0, 1)) == '105'\n\"\"\"\n"""
-    repeats = 10
-    for i in range(repeats):
-        #sentence = input("Enter prompt: ")
-        
-        #tokens_to_generate = int(eval(input("Enter number of tokens to generate: ")))
-        tokens_to_generate = 200
+    samples=[
+        """\"\"\"\nWrite a function to convert the given binary tuple to integer.\nassert binary_to_integer((1, 1, 0, 1, 0, 0, 1)) == '105'\n\"\"\"\n""",
+        """"\"\"\"\nWrite a function to convert the given binary tuple to integer.\nassert binary_to_integer((1, 1, 0, 1, 0, 0, 1)) == '105'\n\"\"\"\n""",
+        """\"\"\"\nWrite a python function to find the cube sum of first n natural numbers.\nassert sum_Of_Series(5) == 225\n\"\"\"\n""",
+        """"\"\"\"\nWrite a python function to find the cube sum of first n natural numbers.\nassert sum_Of_Series(5) == 225\n\"\"\"\n""",
+        ]
+    repeats = 3
+    for sentence in samples:
+        for i in range(repeats):
+            #sentence = input("Enter prompt: ")
+            
+            #tokens_to_generate = int(eval(input("Enter number of tokens to generate: ")))
+            tokens_to_generate = 200
 
-        data = {"prompts": [sentence], "tokens_to_generate": tokens_to_generate}
-        response = requests.put(url, data=json.dumps(data), headers=headers)
+            data = {"prompts": [sentence], "tokens_to_generate": tokens_to_generate}
+            response = requests.put(url, data=json.dumps(data), headers=headers)
 
-        if response.status_code != 200:
-            print(f"Error {response.status_code}: {response.json()['message']}")
-        else:
-            print("Megatron Response: ")
-            print(response.json()['text'][0], flush=True)
-    
+            if response.status_code != 200:
+                print(f"Error {response.status_code}: {response.json()['message']}")
+            else:
+                print("Megatron Response: ")
+                print(response.json()['text'][0], flush=True)
